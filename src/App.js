7e3d75bb559f453672d1a7e3adb7b00db8b6fa60
Choi Youn Seok <cafebom@gmail.com>
2019-11-09 23:33:59 +0900

    https://github.com/younseokc/movie_app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Food({ name, picture }){
+  return <div>
+    <h2>
+      I like {name}
+    </h2>
+    <img src={picture} />
+    </div>
+}
+
+const foodILike = [
+  {
+    name : '김치',
+    image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQg5HKx763vYcezo7tzXrCdVOsvTdQ5Zpe7dmHb_367k1U3leSU'
+  },
+  {
+    name : '돈까스',
+    image : 'http://m.yum-yumfood.com/web/product/big/201607/29_shop1_382074.jpg'
+  }
+]
+
+function App(){
+  return(
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
---
+++ src/potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Potato(){
+    return <h3>I loveotato</h3>
+}
+
+export default Potato;
