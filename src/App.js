305b7c18066ee73820e65fde96ca616f67670fa0
Choi Youn Seok <cafebom@gmail.com>
2019-11-12 00:53:08 +0900

    state와 onclick을 사용하여 콘솔로 찍음

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} />
-    </div>
-    )
-}
-
-const foodILike = [
-  {
-    id:1,
-    name : '김치',
-    image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQg5HKx763vYcezo7tzXrCdVOsvTdQ5Zpe7dmHb_367k1U3leSU',
-    rating :5
-  },
-  {
-    id:2,
-    name : '돈까스',
-    image : 'http://m.yum-yumfood.com/web/product/big/201607/29_shop1_382074.jpg',
-    rating: 4.9
+class App extends React.Component{
+  
+  state ={
+    count : 0
   }
-]
-
-Food.propTypes = {
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-}
-// props의 성격을 나타내주는 것.
-
-function App(){
-  return(
-    <div>
-      {foodILike.map(dish => 
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)}
-    </div>
-  );
-}
+  add =() => {
+    console.log('add')
+  };
+  minus =() => {
+    console.log('minus')
+  };
+  render(){
+    return (
+      <div>
+          <h1>아 힘드네: {this.state.count}</h1>
+          <button onClick={this.add}>ADD</button>
+          <button onClick={this.minus}>Minus</button>
+      </div>
+      );
+    }
+  }
 
 export default App;
